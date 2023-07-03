2c7f61696a969d5a698748d792a91b981b0b546a
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-07-03 19:10:24 -0300

    DOM
    DOM

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 var teste = document.querySelector(".teste");
 
-teste.innerHTML = "Ranyeri";
+teste.innerHTML = "<p>O innerHTML serve para incluir uma propriedade de HTML, ja o innerTEXT é para modificar o texto.</p>";
+
+
 
 
