11f0c8faedd7ff07251252b2d221094aab8d89e9
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-07-03 19:27:46 -0300

    DOM
    Modificando texto e estilo

--- dom.js
+++ dom.js
@@ -1,7 +1,10 @@
 var teste = document.querySelector(".teste");
 
-teste.innerHTML = "<p>O innerHTML serve para incluir uma propriedade de HTML, ja o innerTEXT é para modificar o texto.</p>";
+teste.innerHTML = "<p>O innerHTML serve para incluir uma propriedade de HTML, ja o innerTEXT é para modificar o texto.<br>Para mudar o estilo se seleciona a variavel da tag que quer modificar e depois indica a variavel.style e a popriedade, exemplo estilo.style.backgroundColor = 'red', lembrando que no JS tem que se escrever com camelCase</p>";
 
+let estilo = document.querySelector("body")
 
+estilo.style.backgroundImage = "linear-gradient(to right, red, blue)";
+estilo.style.color = "white";
 
 
