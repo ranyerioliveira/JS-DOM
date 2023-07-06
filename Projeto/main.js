df2f470b0065569971c781a368dea3688e374fbd
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-07-06 09:34:31 -0300

    ajustes
    Ajustes nos arquivos

---
+++ Projeto/main.js
@@ -0,0 +1,30 @@
+
+// ---get all keys (pegar todas as teclas)---
+
+const keys = document.querySelectorAll(".key")
+
+
+//---play notes (tocar notas)---
+//criando uma função
+
+function playNote(event){
+
+   //codigo da tecla
+
+   //tipo da tecla
+
+   //se a tecla existe
+    
+   //play midia  
+
+}
+
+
+// ---click with mouse (clicar o mouse)---
+
+keys.forEach()//forEach siginifa para cada 
+
+// ---keyboard type  (clicar no teclado)---
+//colocando enventos
+window.addEventListener("keydown", playNote) // o keydown serve para que toda vez que focar apertada uma tecla no teclad ele vai rodar uma function
+
